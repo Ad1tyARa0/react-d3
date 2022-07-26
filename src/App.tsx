00094317e0ee3a29aedf4ddd50f37c2799cd1033
Aditya S Rao <aditya.s.rao12@gmail.com>
2022-07-26 23:49:54 +0530

    Rearranged imports.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 
 // Components.
-import { HelloSVG } from "./components/HelloSVG/HelloSVG";
-import { HelloD3Data } from "./components/HelloD3Data/HelloD3Data";
+import { SimpleChartFunctional } from "./components/examples/simple-chart/SimpleChart";
 
 // SCSS.
 import "./App.scss";
-import {
-  SimpleChart,
-  SimpleChartFunctional,
-} from "./components/examples/simple-chart/SimpleChart";
 
 const css_prefix = "app__";
 
