33938c962b8b268a568ece9951948abef0a4d542
Aditya S Rao <aditya.s.rao12@gmail.com>
2022-08-09 21:08:44 +0530

    removed eslint error.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 // Components.
 import { AccentColor } from "./components/AccentColor";
+import { BasicBarChart } from "../../components/charts/basic-bar-chart/BasicBarChart";
 // import { BasicLineChart } from "../../components/charts/basic-line-chart/BasicLineChart";
 // import { BasicAreaChart } from "../../components/charts/basic-area-chart/BasicAreaChart";
 
@@ -10,7 +11,6 @@ import { COLORS } from "../../utils/constants/colors";
 
 // SCSS.
 import "./Home.scss";
-import { BasicBarChart } from "../../components/charts/basic-bar-chart/BasicBarChart";
 
 // Pages -- home
 const css_prefix = "p--h__";
